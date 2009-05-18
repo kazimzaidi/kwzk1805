c0e17cba497a248062e7b13e68616120a844db35
Waseem Ahmad <cougar2149@gmail.com>
2009-05-18 21:10:12 +0530

    Added basic login. Still not tested. No registration yet. Wait for a few more minutes. :)

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,4 @@
 class UserSessionsController < ApplicationController
+  def new
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  before_filter :no_login_required, :only => [:new, :create]
+  before_filter :login_required, :only => [:destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        flash[:notice] = 'New user creation successful.'
+        format.html {redirect_to user_dashboard_url}
+      else
+        render :action => :new
+      end
+    end
+  end
+end
