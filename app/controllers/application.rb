221b7bcde35ee1074ebfc2a2565c84896a0e69ac
Kazim Zaidi <pursuit86@gmail.com>
2009-05-19 07:30:40 +0530

    Fixed current_user in ApplicationController. Modifications to Dashboard.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return @current_user if defined?(@current_user)
-    @current_user = current_user_session and current_user_session.user
+    @current_user = current_user_session.user if current_user_session
   end
 
   def login_required
